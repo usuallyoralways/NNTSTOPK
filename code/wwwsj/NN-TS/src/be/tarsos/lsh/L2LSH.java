9bd473f71f7239b7e617bc737faf582d4531a9ba
lijun <940366838@qq.com>
2019-09-19 21:08:50 +0800

    update

--- code/wwwsj/NN-TS/src/be/tarsos/lsh/L2LSH.java
+++ code/wwwsj/NN-TS/src/be/tarsos/lsh/L2LSH.java
@@ -9,15 +9,53 @@ package be.tarsos.lsh;
  */
 
 
+import be.tarsos.lsh.families.EuclideanDistance;
+import be.tarsos.lsh.families.EuclidianHashFamily;
 import be.tarsos.lsh.families.HashFamily;
 
 import java.util.List;
+import Constant.*;
 
 public class L2LSH extends BashLSH {
+
+
     public L2LSH(List<Vector> dataset, HashFamily hashFamily) {
         super(dataset, hashFamily);
     }
-    public static void main(String[] args){
+    public L2LSH() {
+        super();
 
+    }
+
+    public void initL2LSH(double radius,int dimensions,String datasetPath){
+        setRadius(radius);
+        setDimensions(dimensions);
+        setDatasetPath(datasetPath);
+
+        setFamily();
+        setMeasure();
+        readDataset(false);
+    }
+
+
+    public static void main(String[] args){
+        String filePate= Constant.basePath+"Mnist/Mnist.ds";
+        String queryPate= Constant.basePath+"Mnist/Mnist.q";
+        L2LSH lsh = new L2LSH();
+        lsh.initL2LSH(10,50,filePate);
+        lsh.setQueriesPath(queryPate);
+        lsh.readQueries(false);
+
+    }
+
+    public void setMeasure(){
+        setMeasure(new EuclideanDistance());
+        setFamily();
+    }
+    public void setFamily(){
+        int w = (int) (10 * getRadius());
+        w = w == 0 ? 1 : w;
+        HashFamily hashFamily = new EuclidianHashFamily(w,getDimensions());
+        setFamily(hashFamily);
     }
 }
